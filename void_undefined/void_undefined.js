51f5be70ba1dc86195bbac191a9dabd15982ffed
MarcinBalejko <mbbm9@o2.pl>
2020-04-13 20:10:12 +0200

    :white_check_mark: Added some void and undefined types examples

---
+++ void_undefined/void_undefined.js
@@ -0,0 +1,22 @@
+// void is like an opposite of any, we don't want to return any value at all
+// function logData(): void {
+//   console.log("Here is some data");
+//   return "Hello";  // this will get error
+// }
+// logData();
+// let x: void = null;
+// let u: undefined = undefined;
+// let n: null = null;
+// console.log(n);
+// function error(message: string): never {
+//   throw new Error(message);
+// }
+// error("Something happened");
+// function infLoop(): never {
+//   while (true) {
+//     console.log("Hello");
+//   }
+// }
+function doSomething() {
+    return "Hello";
+}
