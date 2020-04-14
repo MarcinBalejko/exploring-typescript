127ce5a85c65096648df4c9f79c254d8fd49b6f5
MarcinBalejko <mbbm9@o2.pl>
2020-04-14 15:53:01 +0200

    :white_check_mark: Added some destructuring and pattern matching examples

---
+++ destructuring/destructuring.ts
@@ -0,0 +1,34 @@
+// const user = {
+//   first: "John",
+//   last: "Doe",
+// };
+
+// const { first, last } = user;
+
+// console.log(first + " " + last);
+
+// const users = ["mark", "jeff", "carlos"];
+
+// const [user1, user2, user3] = users;
+
+// console.log(user2);
+
+// // Pattern matching
+// const [a, b] = ["x", "y"];
+
+// console.log(a);
+
+// const obj = { a: [{ foo: 123, bar: "abc" }, {}], b: true };
+// const {
+//   a: [{ bar: b }],
+// } = obj;
+
+// // console.log(b);
+
+// const { x: x } = { x: 12, y: 22 };
+// console.log(x);
+
+const [x = 5] = [];
+const { prop: y = 6 } = {};
+console.log(x);
+console.log(y);
