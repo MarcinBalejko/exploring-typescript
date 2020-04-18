1d2aecb6942f04d78fb3e1cca70ef34800259c10
MarcinBalejko <mbbm9@o2.pl>
2020-04-19 00:38:00 +0200

    :white_check_mark: Added Literal Types examples

--- understanding-typescript/basic_types/app.js
+++ understanding-typescript/basic_types/app.js
@@ -1,15 +1,33 @@
 // 1.) UNION TYPES
-function combine(input1, input2) {
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === "number" && typeof input2 === "number") {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result;
+// }
+// const combinedAges = combine(30, 26);
+// console.log(combinedAges);
+// const combinedNames = combine("Max", "Anna");
+// console.log(combinedNames);
+// 2.) LITERAL TYPES
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number" // it has to be exactly what is specified in resultConversion above
+    ) {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Max", "Anna");
+var combinedStringAges = combine("30", 26, "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
--- understanding-typescript/basic_types/app.ts
+++ understanding-typescript/basic_types/app.ts
@@ -1,16 +1,44 @@
 // 1.) UNION TYPES
-function combine(input1: number | string, input2: number | string) {
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === "number" && typeof input2 === "number") {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result;
+// }
+
+// const combinedAges = combine(30, 26);
+// console.log(combinedAges);
+
+// const combinedNames = combine("Max", "Anna");
+// console.log(combinedNames);
+
+// 2.) LITERAL TYPES
+
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number" // it has to be exactly what is specified in resultConversion above
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Anna");
+const combinedStringAges = combine("30", 26, "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
