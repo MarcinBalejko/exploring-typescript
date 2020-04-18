42dad7c6d93dbf2d9630bcaaa83c655e4d94a852
MarcinBalejko <mbbm9@o2.pl>
2020-04-18 22:30:32 +0200

    :white_check_mark: Added some object and array types examples

--- understanding-typescript/basic_types/app.js
+++ understanding-typescript/basic_types/app.js
@@ -20,17 +20,47 @@
 // const result = add(number1, number2);
 // console.log(result);
 // 2. BOOLEANS:
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+// const number1 = 5; // 5.0
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = "Result is: ";
+// const result = add(number1, number2, printResult, resultPhrase);
+// 3. TYPE ASSIGNMENT & TYPE INFERENCE
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+// let number1: number; // stating that eventually a number will be here
+// number1 = 5; // 5.0
+// const number2 = 2.8;
+// const printResult = true;
+// let resultPhrase = "Result is: ";
+// resultPhrase = 0;
+// const result = add(number1, number2, printResult, resultPhrase);
+//4. OBJECT TYPES & ARRAY TYPES
+// {} = same as ... : object
+var person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
+};
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
 }
-var number1 = 5; // 5.0
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-var result = add(number1, number2, printResult, resultPhrase);
--- understanding-typescript/basic_types/app.ts
+++ understanding-typescript/basic_types/app.ts
@@ -32,18 +32,58 @@
 
 // 2. BOOLEANS:
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// const number1 = 5; // 5.0
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = "Result is: ";
+
+// const result = add(number1, number2, printResult, resultPhrase);
+
+// 3. TYPE ASSIGNMENT & TYPE INFERENCE
+
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// let number1: number; // stating that eventually a number will be here
+// number1 = 5; // 5.0
+// const number2 = 2.8;
+// const printResult = true;
+// let resultPhrase = "Result is: ";
+// resultPhrase = 0;
+
+// const result = add(number1, number2, printResult, resultPhrase);
+
+//4. OBJECT TYPES & ARRAY TYPES
+
+// {} = same as ... : object
+
+const person = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
 }
-
-const number1 = 5; // 5.0
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
