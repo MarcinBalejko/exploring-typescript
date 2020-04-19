bfd5619f3a3796e1fee9a8082f4974bede6147d9
MarcinBalejko <mbbm9@o2.pl>
2020-04-19 22:03:47 +0200

    :white_check_mark: Added Never and Unknown types examples

--- understanding-typescript/basic_types/app.js
+++ understanding-typescript/basic_types/app.js
@@ -70,16 +70,58 @@
 // }
 // printResult(add(5, 12));
 // 5.) FUNCTIONS AS TYPES
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log("Result: " + num);
-}
-printResult(add(5, 12));
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+// printResult(add(5, 12));
 // combineValues should take any function that takes 2 parameters which are numbers and which returns number
-var combineValues;
-combineValues = add;
+// let combineValues: (a: number, b: number) => number;
+// combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;   // we would get error cause 5 is not a function
-console.log(combineValues(8, 8));
+// console.log(combineValues(8, 8));
+// 6.) FUNCTION TYPES & CALLBACKS
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+// function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+//   const result = n1 + n2;
+//   cb(result);
+// }
+// printResult(add(5, 12));
+// let combineValues: (a: number, b: number) => number;
+// combineValues = add;
+// // combineValues = printResult;
+// // combineValues = 5;
+// console.log(combineValues(8, 8));
+// addAndHandle(10, 20, (result) => {
+//   console.log(result);
+// });
+// 7.) THE UNKNOWN TYPE
+// let userInput: unknown;
+// let userName: string;
+// userInput = 5;
+// userInput = "Max";
+// // userName = userInput; // this would throw an error
+// if (typeof userInput === "string") {
+//   userName = userInput;
+// }
+// 8.) THE NEVER TYPE
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+function generateError(message, code) {
+    // this function is intended to *never return anything
+    throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
--- understanding-typescript/basic_types/app.ts
+++ understanding-typescript/basic_types/app.ts
@@ -89,22 +89,81 @@
 
 // 5.) FUNCTIONS AS TYPES
 
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
 
-printResult(add(5, 12));
+// printResult(add(5, 12));
 
 // combineValues should take any function that takes 2 parameters which are numbers and which returns number
-let combineValues: (a: number, b: number) => number;
+// let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
+// combineValues = add;
 // combineValues = printResult;
 
 // combineValues = 5;   // we would get error cause 5 is not a function
 
-console.log(combineValues(8, 8));
+// console.log(combineValues(8, 8));
+
+// 6.) FUNCTION TYPES & CALLBACKS
+
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+
+// function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+//   const result = n1 + n2;
+//   cb(result);
+// }
+
+// printResult(add(5, 12));
+
+// let combineValues: (a: number, b: number) => number;
+
+// combineValues = add;
+// // combineValues = printResult;
+// // combineValues = 5;
+
+// console.log(combineValues(8, 8));
+
+// addAndHandle(10, 20, (result) => {
+//   console.log(result);
+// });
+
+// 7.) THE UNKNOWN TYPE
+
+// let userInput: unknown;
+// let userName: string;
+
+// userInput = 5;
+// userInput = "Max";
+// // userName = userInput; // this would throw an error
+// if (typeof userInput === "string") {
+//   userName = userInput;
+// }
+
+// 8.) THE NEVER TYPE
+
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  // this function is intended to *never return anything
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occurred!", 500);
