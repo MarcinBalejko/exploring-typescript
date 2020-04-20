3701acfa22dfe08271babed0a6c308e7e60795ca
MarcinBalejko <mbbm9@o2.pl>
2020-04-20 23:50:11 +0200

    :white_check_mark: Added inheritance example

--- understanding-typescript/classes/app.js
+++ understanding-typescript/classes/app.js
@@ -2,6 +2,19 @@
 // class Department {
 //   name: string;
 //   private employees: string[] = [];
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //   constructor(n: string) {
 //     this.name = n;
 //   }
@@ -25,17 +38,33 @@
 // accounting.describe();
 // accounting.printEmployeeInformation();
 // 2.) SHORTHAND INITIALIZATION & READONLY
+// class Department {
+//   // private id: string
+//   // private name: string;
+//   private employees: string[] = [];
+//   // *readonly is like attr_reader in ruby
+//   // it ensures that this property does not get changed
+//   constructor(private readonly id: string, public name: string) {
+//     // this.id = id
+//     // this.name = n;
+//   }
+//   describe(this: Department) {
+//     console.log(`Department (${this.id}): ${this.name}`);
+//   }
+//   addEmployee(employee: string) {
+//     this.employees.push(employee);
+//   }
+//   printEmployeeInformation() {
+//     console.log(this.employees.length);
+//     console.log(this.employees);
+//   }
+// }
+// 3.) INHERITANCE
 var Department = /** @class */ (function () {
-    // *readonly is like attr_reader in ruby
-    // it ensures that this property does not get changed
     function Department(id, name) {
         this.id = id;
         this.name = name;
-        // private id: string
-        // private name: string;
         this.employees = [];
-        // this.id = id
-        // this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department (" + this.id + "): " + this.name);
@@ -49,3 +78,37 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Manu");
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+console.log(it);
+var accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Something went wrong...");
+accounting.printReports();
