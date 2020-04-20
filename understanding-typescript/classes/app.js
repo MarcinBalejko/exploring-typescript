4c3d49cc731fd6285c4ec690097924bee02607c9
MarcinBalejko <mbbm9@o2.pl>
2020-04-20 22:39:57 +0200

    :white_check_mark: Added shorthand initialization and readonly example

--- understanding-typescript/classes/app.js
+++ understanding-typescript/classes/app.js
@@ -1,10 +1,44 @@
+// 1.) THIS
+// class Department {
+//   name: string;
+//   private employees: string[] = [];
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+//   describe(this: Department) {
+//     console.log("Department: " + this.name);
+//   }
+//   addEmployee(employee: string) {
+//     this.employees.push(employee);
+//   }
+//   printEmployeeInformation() {
+//     console.log(this.employees.length);
+//     console.log(this.employees);
+//   }
+// }
+// const accounting = new Department("Accounting");
+// accounting.addEmployee("Max");
+// accounting.addEmployee("Manu");
+// accounting.employees[2] = "Anna";
+// we attached 'private' to employees above so that this way doesn't work
+// it would still be executed in the js runtime though
+// accounting.describe();
+// accounting.printEmployeeInformation();
+// 2.) SHORTHAND INITIALIZATION & READONLY
 var Department = /** @class */ (function () {
-    function Department(n) {
+    // *readonly is like attr_reader in ruby
+    // it ensures that this property does not get changed
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // private id: string
+        // private name: string;
         this.employees = [];
-        this.name = n;
+        // this.id = id
+        // this.name = n;
     }
     Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
+        console.log("Department (" + this.id + "): " + this.name);
     };
     Department.prototype.addEmployee = function (employee) {
         this.employees.push(employee);
@@ -15,11 +49,3 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department("Accounting");
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-// accounting.employees[2] = "Anna";
-// we attached 'private' to employees above so that this way doesn't work
-// it would still be executed in the js runtime though
-accounting.describe();
-accounting.printEmployeeInformation();
--- understanding-typescript/classes/app.ts
+++ understanding-typescript/classes/app.ts
@@ -1,13 +1,54 @@
+// 1.) THIS
+// class Department {
+//   name: string;
+//   private employees: string[] = [];
+
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+
+//   describe(this: Department) {
+//     console.log("Department: " + this.name);
+//   }
+
+//   addEmployee(employee: string) {
+//     this.employees.push(employee);
+//   }
+
+//   printEmployeeInformation() {
+//     console.log(this.employees.length);
+//     console.log(this.employees);
+//   }
+// }
+
+// const accounting = new Department("Accounting");
+
+// accounting.addEmployee("Max");
+// accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "Anna";
+// we attached 'private' to employees above so that this way doesn't work
+// it would still be executed in the js runtime though
+
+// accounting.describe();
+// accounting.printEmployeeInformation();
+
+// 2.) SHORTHAND INITIALIZATION & READONLY
+
 class Department {
-  name: string;
+  // private id: string
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  // *readonly is like attr_reader in ruby
+  // it ensures that this property does not get changed
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -19,15 +60,3 @@ class Department {
     console.log(this.employees);
   }
 }
-
-const accounting = new Department("Accounting");
-
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-
-// accounting.employees[2] = "Anna";
-// we attached 'private' to employees above so that this way doesn't work
-// it would still be executed in the js runtime though
-
-accounting.describe();
-accounting.printEmployeeInformation();
