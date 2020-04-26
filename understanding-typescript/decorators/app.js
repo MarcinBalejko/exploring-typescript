720bba3ec5da6652211f02d866237fc832355ec3
MarcinBalejko <mbbm9@o2.pl>
2020-04-27 00:10:30 +0200

    :construction: Implementing input validation with decorators

--- understanding-typescript/decorators/app.js
+++ understanding-typescript/decorators/app.js
@@ -6,81 +6,56 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// capital letter is optional here
-// function Logger(constructor: Function) {
-//   console.log("Logging...");
-//   console.log(constructor);
-// }
-// @Logger
-// class Person {
-//   name = "Max";
-//   constructor() {
-//     console.log("Creating person object...");
-//   }
-// }
-// const pers = new Person();
-// console.log(pers);
-// 2.) DECORATOR FACTORY
-// function Logger(logString: string) {
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
-// @Logger("LOGGING - PERSON") // the idea is that now we can pass in the values that our inner decorator function above will use
-// class Person {
-//   name = "Max";
-//   constructor() {
-//     console.log("Creating person object...");
-//   }
-// }
-// const pers = new Person();
-// console.log(pers);
-// 3.) MORE USEFUL DECORATOR
-// function WithTemplate(template: string, hookId: string) {
-//   return function (constructor: any) {
-//     const hookEl = document.getElementById(hookId);
-//     const p = new constructor();
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector("h1")!.textContent = p.name;
-//     }
-//   };
-// }
-// // @Logger('LOGGING - PERSON')
-// @WithTemplate("<h1>My Person Object</h1>", "app")
-// class Person {
-//   name = "Max";
-//   constructor() {
-//     console.log("Creating person object...");
-//   }
-// }
-// const pers = new Person();
-// console.log(pers);
-// 4.) AUTOBIND DECORATOR
-function Autobind(_, _2, descriptor) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor = {
-        configurable: true,
-        enumerable: false,
-        get() {
-            const boundFn = originalMethod.bind(this); // here 'this' will always refer to the object that we defined 'get' on
-            return boundFn;
-        },
-    };
-    return adjDescriptor; // this will overwrite the old descriptor
+const registeredValidators = {};
+function Required(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ["required"] });
 }
-class Printer {
-    constructor() {
-        this.message = "This works!";
+function PositiveNumber(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ["positive"] });
+}
+function validate(obj) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
     }
-    showMessage() {
-        console.log(this.message);
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case "required":
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case "positive":
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+class Course {
+    constructor(t, p) {
+        this.title = t;
+        this.price = p;
     }
 }
 __decorate([
-    Autobind
-], Printer.prototype, "showMessage", null);
-const p = new Printer();
-const button = document.querySelector("button");
-button.addEventListener("click", p.showMessage);
+    Required
+], Course.prototype, "title", void 0);
+__decorate([
+    PositiveNumber
+], Course.prototype, "price", void 0);
+const courseForm = document.querySelector("form");
+courseForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titleEl = document.getElementById("title");
+    const priceEl = document.getElementById("price");
+    const title = titleEl.value;
+    const price = +priceEl.value;
+    const createdCourse = new Course(title, price);
+    if (!validate(createdCourse)) {
+        alert("Invalid input, please try again!");
+        return;
+    }
+    console.log(createdCourse);
+});
--- understanding-typescript/decorators/app.ts
+++ understanding-typescript/decorators/app.ts
@@ -70,29 +70,104 @@
 
 // 4.) AUTOBIND DECORATOR
 
-function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
-  const adjDescriptor: PropertyDescriptor = {
-    configurable: true,
-    enumerable: false,
-    get() {
-      const boundFn = originalMethod.bind(this); // here 'this' will always refer to the object that we defined 'get' on
-      return boundFn;
-    },
+// function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+//   const originalMethod = descriptor.value;
+//   const adjDescriptor: PropertyDescriptor = {
+//     configurable: true,
+//     enumerable: false,
+//     get() {
+//       const boundFn = originalMethod.bind(this); // here 'this' will always refer to the object that we defined 'get' on
+//       return boundFn;
+//     },
+//   };
+//   return adjDescriptor; // this will overwrite the old descriptor
+// }
+
+// class Printer {
+//   message = "This works!";
+
+//   @Autobind
+//   showMessage() {
+//     console.log(this.message);
+//   }
+// }
+
+// const p = new Printer();
+
+// const button = document.querySelector("button")!;
+// button.addEventListener("click", p.showMessage);
+
+// 5.) VALIDATION WITH DECORATORS
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
   };
-  return adjDescriptor; // this will overwrite the old descriptor
 }
 
-class Printer {
-  message = "This works!";
+const registeredValidators: ValidatorConfig = {};
 
-  @Autobind
-  showMessage() {
-    console.log(this.message);
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["required"],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["positive"],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
   }
 }
 
-const p = new Printer();
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
 
-const button = document.querySelector("button")!;
-button.addEventListener("click", p.showMessage);
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
